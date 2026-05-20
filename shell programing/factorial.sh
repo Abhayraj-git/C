7493b0154a9f1430a26732335b11364c40857377
Abhayraj-git <abhayrajguptakk@gmail.com>
2026-05-20 12:09:25 +0530

    Delete shell programing directory

--- shell programing/factorial.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enetr the value of n : "
-read n
-factorial=1
-for ((i=1;i<=n;i++))
-do
-factorial=$((factorial*i))
-done
-echo "$factorial"
--- shell programing/temprature.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Enter the temprature in farnehite : "
-read fernehite 
-celsius=$(echo "scale=2; ($fernehite - 32) * 5 / 9" | bc)
-echo "$celsius"
